c507ee3a91af75aeea833498f526452edd8f9e3d
Alejandro Venegas <avenegas@transbank.cl>
2021-07-26 16:00:17 -0400

    feat: added windows 10 notification when world status changed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # neworld-checkhealth
 Script to get windows10 notification if your world status change (up or down) in newworld amazon game
+
+# Execute 'py newworld_serverstatus.py'
+Keep the console open
---
+++ newworld_serverstatus.py
@@ -0,0 +1,27 @@
+# import lib
+import requests
+import time
+
+# Endpoint
+from win10toast import ToastNotifier # notifier
+toaster = ToastNotifier()
+URL = "https://www.newworld.com/es-es/support/server-status"
+SERVER = "Machina"
+changed = False # first time call
+print("We will notify you when there is a change in your world, to finish press CTRL + C")
+
+# Start of ouroboro
+while True :
+  
+ r = requests.get(url = URL) # call the page
+ 
+ txt = r.content.decode('utf-8') # server response as string
+ indice = txt.find(SERVER) # search the server in the response
+ status_substr = txt[indice-200:indice].find("up") # if find up index then is server up, down also can be used
+ up = status_substr > 0 # -1 if changed
+ if up != changed: # status changed after last change
+  notif =  "Status Changed:" + SERVER +" "+ str(up) 
+  toaster.show_toast("NEW WORLD SERVER", notif)
+  changed = up
+  print("Notification sended",changed)
+ time.sleep( 5 ) # check every 5 secs
